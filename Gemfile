1341dbfafe67ab45c74b214248254f9be442f734
Kenneth <github@kennethcalamay.com>
2012-12-17 14:38:30 +0800

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'sqlite3', group: :development
+gem 'pg', group: :production
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'rspec-rails', '>= 2.0.1'
+  gem 'capybara'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'launchy'
+end
+
+gem 'jquery-rails'
+gem 'devise'
+gem 'slim'
+gem 'slim-rails'
+gem 'inherited_resources'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chefroast::Application.config.secret_token = '1d333e9d5ebfcd56352f1e25471ba883466afc6f5c771804065d6a962f733409cfbe62dd096408fe21448d9f036cab39e192dc2e33ed224e8179d0321aba5361'

730b797b87c5d6efe0f0d7c174da894ed83717c8
Kenneth <github@kennethcalamay.com>
2012-12-20 15:30:23 +0800

    Added products model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Chefroast::Application.routes.draw do
   devise_for :users
+  resources :products
   root to: "products#index"
 end
---
+++ db/migrate/20121220072725_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :image
+      t.string :name
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end

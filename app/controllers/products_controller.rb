a57d9efcc81dbc18ce9289119e664fb0e880612d
Kenneth <github@kennethcalamay.com>
2013-06-16 02:13:04 +0800

    Action caching

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
+  caches_action :index
+
   before_filter :authenticate_user!, except: [:index, :show]
   inherit_resources
 
   def index
-    @products = Product.all_cached
+    @products = Product.all
   end
 
   def show
@@ -21,11 +23,20 @@ class ProductsController < ApplicationController
 
   def update
     update! and return unless params[:commit] == 'Publish'
+
     @product = Product.find(params[:id])
     @product.update_attributes(params[:product])
     @product.publish!
-    flash[:notice] = "Your product has been published!" unless @product.errors.any?
+    unless @product.errors.any?
+      flash[:notice] = "Your product has been published!"
+      expire_action action: :index
+    end
     respond_with @product
+  end
+
+  def destroy
+    expire_action action: :index
+    destroy!
   end
 
   def drafts
